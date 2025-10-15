0bb09c3e69d16a37cff6a9cfb8e8b6020dfa1191
Vinay Siddha <121566763+VinaySiddha@users.noreply.github.com>
2025-10-15 13:04:30 +0530

    Delete trevon directory

--- trevon/yasha/models.py
+++
@@ -1,6 +0,0 @@
-# some_app/models.py
-from django.conf import settings
-from accounts.models import *
-
-class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
